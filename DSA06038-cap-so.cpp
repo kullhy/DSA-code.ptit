a5b9d4e9bcb6a1825ab95b06b151b225f3afcdaa
kullnopro <92315061+kullnopro@users.noreply.github.com>
2022-03-18 16:18:12 +0700

    Update DSA06038-cap-so.cpp

--- DSA06038-cap-so.cpp
+++ DSA06038-cap-so.cpp
@@ -1,31 +1,44 @@
 #include <bits/stdc++.h>
 #define endl "\n"
 using namespace std;
+int bit[1000001];
+void update(int i)
+{
+    for (; i < 1000001; i += i & -i)
+        bit[i]++;
+}
+int get(int i)
+{
+    int s = 0;
+    for (; i; i -= i & -i)
+        s += bit[i];
+    return s;
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int t;
-    cin >> t;
-    while (t--)
+    int n, ans = 0;
+    cin >> n;
+    int a[n + 1];
+    stack<int> st;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = n; i > 0; i--)
     {
-        int n;
-        cin >> n;
-        int l[n], maz = 0;
-        vector<pair<int, int>> a(n);
-        for (int i = 0; i < n; i++)
-            cin >> a[i].first >> a[i].second;
-        sort(a.begin(), a.end());
-        for (int i = 0; i < n; i++)
+        if (a[i] % 2 == 0)
         {
-            l[i] = 1;
-            for (int j = 0; j < i; j++)
-            {
-                if (a[i].first > a[j].second && l[i] <= l[j])
-                    l[i] = l[j] + 1;
-            }
-            maz = max(maz, l[i]);
+            ans += get(a[i] - 1);
+            st.push(a[i]);
         }
-        cout << maz << endl;
+        else
+        {
+            while (!st.empty())
+            {
+                update(st.top());
+                st.pop();
+            }
+        }
     }
+    cout << ans;
 }
